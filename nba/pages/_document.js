25e39afca38a5bd978f63a296af888d7b6aa7f4d
Omar Abdiwali <omarabdiwali17@gmail.com>
2026-03-28 23:02:12 -0600

    adding seo

--- nba/pages/_document.js
+++ nba/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="FT-zhFm6Eyxo29YZJaAICTigJzNgDzoWVue_2yv7cSs" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
